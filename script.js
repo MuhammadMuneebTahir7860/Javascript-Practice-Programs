e5deca69d9acaef48c31efb2aea1b05616f8ce83
MuhammadMuneebTahir7860 <muhammadmuneebtahir7860@gmail.com>
2021-03-05 00:21:39 -0800

    All 42 Programs of Assignment 15

--- script.js
+++ script.js
@@ -236,7 +236,28 @@ const checkPalindrome = () =>{
 
 const reverseWords = () =>{
     var stringWordReverseInput=document.getElementById("stringWordReverseInput").value;
-
+    var reverseWordsString="";
+    var reverseLine="";
+    while(stringWordReverseInput.match("  ")){
+        stringWordReverseInput=stringWordReverseInput.replace("  "," ")
+    }
+    for(i=stringWordReverseInput.length-1; i>=0; i--){
+        if(stringWordReverseInput.slice(i,i+1) !=" "){
+            reverseWordsString =stringWordReverseInput[i] + reverseWordsString;
+        }
+        else{
+            if(reverseLine==""){
+                reverseLine=reverseWordsString;
+                reverseWordsString="";
+            }
+            else{
+                reverseLine=reverseLine + " " + reverseWordsString;
+                reverseWordsString= ""; 
+            }
+        }
+    }
+    reverseLine=reverseLine + " " + reverseWordsString;
+    document.getElementById("stringWordReverseOutput").innerHTML=`Old String = ${stringWordReverseInput} AND Reversed String = ${reverseLine}`;  
 }
 
 const checkIndex = () =>{
@@ -259,9 +280,237 @@ const lastCharIndex = () =>{
 const arrayHandling = () =>{
     var numberOfElements=document.getElementById("numberOfElements").value;
     var arr=[];
-    let t=arr;
     for(i=0; i<numberOfElements; i++){
-      t[i]=prompt("Enter Array Element");
+      arr[i]=prompt("Enter Array Element");
     }
-    document.getElementById("arrayOutPut").innerHTML=`Array = ${t}`;
+    document.getElementById("arrayOutPut").innerHTML=`Array = ${arr}`;
+}
+
+const findingNegative = () =>{
+    var negativeInput=document.getElementById("negativeInput").value;
+    var fullllllArray=[];
+    var array="";
+    for(i=0; i<negativeInput; i++){
+        fullllllArray[i]=prompt("Enter Number");
+        if(fullllllArray[i]<0){
+            array =fullllllArray[i]  +  "," + array;
+        }
+    }
+    document.getElementById("negativeOutput").innerHTML=`Array = ${fullllllArray} And Negative Array = ${array}`;
+}
+
+const sumButton = () =>{
+    var sumInput=document.getElementById("sumInput").value;
+    var sumArray=[];
+    var sumResult=0;
+    for(i=0; i<sumInput; i++){
+        sumArray[i]=+prompt("Enter Number");
+        sumResult = sumResult + parseFloat(sumArray[i]);
+    }
+    document.getElementById("sumOutput").innerHTML=`Sum of Array ${sumArray} = ${sumResult}`;
+}
+
+const maxMin = () =>{
+    var maxMinInput=document.getElementById("maxMinInput").value;
+    var maxMinArray=[];
+    var max=0;
+    var min=[];
+    parseInt(min);
+    for(i=0; i<maxMinInput; i++){
+        maxMinArray[i]=parseInt(prompt("Enter any Number"));
+        if(parseInt (maxMinArray[i])>max){
+            max=parseInt(maxMinArray[i]);
+        }
+        min[0]=parseInt(maxMinArray[0]);
+        if(parseInt(maxMinArray[i])<min){
+            min=parseInt(maxMinArray[i]);
+        }
+    }
+    document.getElementById("maxMinOutput").innerHTML=`Array = ${maxMinArray} Maximum Number = ${max} Mimum Number = ${min}`;
+}
+
+const secondLargetNumber = () =>{
+    var arrayElementsInput=document.getElementById("arrayElementsInput").value;
+    var elementsArray=[];
+    var largest=0;
+    var secondLarget=0;
+    for(i=0; i<arrayElementsInput; i++){
+        elementsArray[i]=parseInt(prompt("Enter any Number"));
+        if(elementsArray[i]>largest){
+            largest=elementsArray[i];
+        }
+    }
+    elementsArray.splice(elementsArray.indexOf(largest),1);
+    console.log(elementsArray);
+    for(i=0; i<=elementsArray.length; i++){
+        if(elementsArray[i]>secondLarget){
+            secondLarget=elementsArray[i];
+        }
+    }
+    document.getElementById("secondLargestOutput").innerHTML=`Second Largest Number of Array = ${secondLarget}`;
+}
+
+const checkEvenOdd = () =>{
+    var numberOfElementsInput=document.getElementById("numberOfElementsInput").value;
+    var totalArrayElements=[];
+    var even=0;
+    var odd=0;
+    for(i=0; i<numberOfElementsInput; i++){
+        totalArrayElements[i]=parseInt(prompt("Enter any Number"));
+        if(totalArrayElements[i]%2==0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+    document.getElementById("evenOddOutput").innerHTML=`Total Even Number in Array = ${even} and Total Odd Number in Array = ${odd}`;
+
+}
+
+const negativPostiveCheck = () =>{
+    var negativePositiveInput=document.getElementById("negativePositiveInput").value;
+    var arrayFull=[];
+    var negative=0;
+    for(i=0; i<negativePositiveInput; i++){
+        arrayFull[i]=parseInt(prompt("Enter Any Number"));
+        if(arrayFull[i]<0){
+            negative++;
+        }
+    }
+    document.getElementById("negativePositiveOutput").innerHTML=`Total Negative Number = ${negative};`
+}
+
+const copyArray = () =>{
+    var firstArrayInput=document.getElementById("firstArrayInput").value;
+    var secondArrayInput=document.getElementById("secondArrayInput").value;
+    var copyArray=[firstArrayInput,secondArrayInput];
+    document.getElementById("copyArrayOutput").innerHTML=`First Array = ${firstArrayInput} And Sceond Array = ${secondArrayInput} Copy Array = ${copyArray}`;
+}
+
+const insertElement= () =>{
+    var fullarrayInput=document.getElementById("fullarrayInput").value;
+    var insertInput=document.getElementById("insertInput").value;
+    var inputArray=[];
+    var w="";
+    var newFullArray=[];
+    for(i=0; i<fullarrayInput.length; i++){
+        if(fullarrayInput.slice(i,i+1)!=","){
+            w=w+fullarrayInput[i];
+        }
+        else if(w!=""){
+            inputArray.push(w);
+            w="";
+        }
+    }
+     inputArray.push(w);
+    newFullArray=inputArray;
+    newFullArray.push(insertInput);
+    document.getElementById("insertOutput").innerHTML=`First Array = ${fullarrayInput} Insert Element = ${insertInput} And New Array After Insertion = ${newFullArray}`;
+}
+
+const deleteElement = () =>{
+    var elementsInput=document.getElementById("elementsInput").value;
+    var deleteInput=document.getElementById("deleteInput").value;
+    var elementsFullArray=[];
+    var h="";
+    var newArrayAfterDel=[];
+    for(i=0; i<elementsInput.length; i++){
+        if(elementsInput.slice(i,i+1) !=","){
+            h=h+elementsInput[i];
+        }
+        else if(h!=""){
+            elementsFullArray.push(h);
+            h="";
+        }
+    }
+    elementsFullArray.push(h);
+    newArrayAfterDel=elementsFullArray;
+    newArrayAfterDel.splice(deleteInput,1);
+    document.getElementById("afterDelOutput").innerHTML=`Position = ${deleteInput} After Deletion Array = ${newArrayAfterDel}`;
+}
+
+
+// Array Handling Programs End
+
+const roundNumber = () =>{
+    var numberInput=document.getElementById("numberInput1").value;
+    var integerConvert=parseInt(numberInput);
+    var newNumber=integerConvert + .50;
+    var realAnswer=0;
+    if(numberInput >= newNumber){
+        realAnswer=integerConvert+1;
+    }
+    else{
+        realAnswer=integerConvert;
+    }
+    document.getElementById("roundOutput").innerHTML=`Round Number = ${realAnswer}`;
+}
+
+const ceilNumber = () =>{
+    var ceilNumberInput=document.getElementById("ceilNumberInput").value;
+    var ceilInteger=parseInt(ceilNumberInput);
+    var newCeilAnswer=0;
+        if(ceilNumberInput.match(/["."]/gi)){
+            newCeilAnswer=ceilInteger+1;
+        }
+        else{
+            newCeilAnswer=ceilInteger;
+        }
+    document.getElementById("ceilNumberOutput").innerHTML=`Ceil Number = ${newCeilAnswer}`;
+}
+
+const floorNumber = () =>{
+    var floorInput=document.getElementById("floorInput").value;
+    var integerFloor=parseInt(floorInput);
+    document.getElementById("floorOutput").innerHTML=`Floor Value = ${integerFloor}`;
+}
+
+// Math Methods End
+
+const printNaturalNumbers = () =>{
+    var naturalNumberInput=document.getElementById("naturalNumberInput").value;
+    var naturalNumbers="";
+    for(i=1; i<=naturalNumberInput; i++){
+        naturalNumbers =naturalNumbers + "," + i;
+    }
+    document.getElementById("naturalNumberOutput").innerHTML=`${naturalNumbers}`;
+}
+
+const naturalNumberPrint = () =>{
+    var InPutNaturalNumber=document.getElementById("InPutNaturalNumber").value;
+    var emptyvar="";
+    for(i=InPutNaturalNumber; i>=1; i--){
+        emptyvar=emptyvar + "," + i;
+    }
+    document.getElementById("outPutNaturalNumber").innerHTML=emptyvar;
+}
+
+const printAlphabets = () =>{
+    var abc="";
+    var alphabetsArry= [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`]
+    for(i=0; i<alphabetsArry.length; i++){
+        abc= abc + "," + alphabetsArry[i];
+    }
+    document.getElementById("alphabestOutPut").innerHTML=abc;
+}
+
+const printEvenNumbers = () =>{
+    var evenNumber="";
+    for(i=1; i<=100; i++){
+        if(i%2==0){
+        evenNumber =evenNumber + "," +  i;
+        }
+    }
+    document.getElementById("allEvenNumbersOutput").innerHTML=evenNumber;
+}
+
+const printOddNumbers = () =>{
+    var oddNumber="";
+    for(i=1; i<=100; i++){
+        if(i%2 !=0){
+            oddNumber = oddNumber + "," + i;
+        }
+    }
+    document.getElementById("allOddNumbersOutput").innerHTML=oddNumber;
 }
